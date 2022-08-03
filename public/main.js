183c2fc8ee18ebaa56733a6d027a4386088ff211
James Tennant <jtennant@ucsc.edu>
2022-08-02 17:50:51 -0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,73 @@
+const socket = io();
+
+let gamestate;
+
+socket.on('update', gs => gamestate = gs);
+
+// const shrink = 800 / window.innerWidth / 2;
+// const shrink = 4;
+const shrink = 1;
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+canvas.width = 800 / shrink;
+canvas.height = 600 / shrink;
+
+
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+img.src = 'player.svg';
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  if (!gamestate) return;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  let ball = true;
+  for (const body of gamestate) {
+    let {x, y, r} = body;
+    x /= shrink;
+    y /= shrink;
+    if (ball) {
+      ctx.beginPath();
+      ctx.arc(x, y, 50 / shrink, 0, 2 * Math.PI);
+      ctx.fill();
+      ball = false;
+      continue;
+    }
+    // draw player based on x, y, r
+    let w = 60;
+    let h = 80;
+    w /= shrink;
+    h /= shrink;
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(r);
+    ctx.translate(-w/2, -h/1.5);
+    ctx.drawImage(img, 0, 0, w, h);
+    ctx.restore();
+  }
+}
+
+animate();
+
+const rotate = document.createElement('button');
+const translate = document.createElement('button');
+
+rotate.textContent = 'rotate';
+translate.textContent = 'translate';
+
+document.body.appendChild(rotate);
+document.body.appendChild(translate);
+
+translate.onpointerdown = rotate.onpointerdown = (e) => input(e, true);
+translate.onpointerup = rotate.onpointerup = (e) => input(e, false);
+
+function input(e, down) {
+  e.target.style.opacity = down ? 0.5 : 1;
+  let code = e.target.textContent === 'translate' ? 't' : 'r';
+  if (!down) code = code.toUpperCase();
+  socket.volatile.emit('input', code);
+}
---
+++ src/server.js
@@ -0,0 +1,90 @@
+// The server mainly manages the physics world
+// It adds and removes players from physics world
+// It sets the rules
+// It responds to input and influences the physics world accordingly
+// It extracts and broadcasts the gamestate
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const { Engine, Runner, Bodies, Events,
+  Composite, Vertices, Body } = require('matter-js');
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public')); // serve client
+
+const engine = Engine.create(); // create an engine
+engine.gravity.scale *= 0.5;
+
+// create walls
+Composite.add(engine.world, [
+  Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+  Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+  Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+]);
+
+// create ball
+const options = { mass: 0.1, restitution: 0.9 };
+const ball = Bodies.circle(100, 100, 50, options);
+Composite.add(engine.world, [ball]);
+
+var runner = Runner.create(); // create runner
+
+setTimeout(() => (
+  Runner.run(runner, engine)
+), 1000);
+
+let playerCount = 0;
+
+// listen for connection
+io.on('connection', (socket) => {
+  console.log(`User connected! Total: ${++playerCount}`);
+
+  const arrow = Vertices.fromPath('0 80 30 0 60 80');
+  const body = Bodies.fromVertices(500, 100, arrow, { friction: 0.01 });
+  Composite.add(engine.world, [body]);
+
+  let translate = false;
+  let rotate = false;
+
+  Events.on(engine, 'beforeUpdate', () => {
+    body.torque = rotate ? 0.1 : 0;
+    if (!translate) return;
+    const pos = body.position;
+    Body.applyForce(body, pos, {
+      x: 0.005 * Math.sin(body.angle),
+      y: -0.005 * Math.cos(body.angle),
+    });
+  });
+
+  socket.on('input', code => {
+    switch (code) {
+      case 't': translate = true; break;
+      case 'T': translate = false; break;
+      case 'r': rotate = true; break;
+      case 'R': rotate = false; break;
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected! Total: ${--playerCount}`);
+    Composite.remove(engine.world, [body]);
+  });
+});
+
+// emit regular updates to all clients 
+setInterval(() => {
+  const players = engine.world.bodies.slice(4); // exclude 4 walls
+  const gamestate = players.map(body => {
+    return {
+      x: Math.round(body.position.x),
+      y: Math.round(body.position.y),
+      r: Math.round(body.angle * 100) / 100,
+    };
+  })
+  io.volatile.emit('update', gamestate);
+}, 1000 / 30);
+
+http.listen(port, () => console.log('listening on port ' + port));
